bafa5909d9210cbd0a11bb56aab077b48fe7527e
Ajay Vohra <ajayvohr@amazon.com>
2019-09-11 04:55:22 +0000

    update nvidia-device-plugin

--- eks-cluster/apply-nvidia-plugin.sh
+++ eks-cluster/apply-nvidia-plugin.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v1.11/nvidia-device-plugin.yml
-
+kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/master/nvidia-device-plugin.yml
 kubectl get nodes "-o=custom-columns=NAME:.metadata.name,GPU:.status.allocatable.nvidia\.com/gpu"
