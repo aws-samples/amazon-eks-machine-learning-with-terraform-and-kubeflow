340e368d8d3b2f6b49edb08d6536cc84331b94f2
Ajay Vohra <ajayvohr@amazon.com>
2021-03-05 18:19:45 +0000

    remove obsolete scripts

--- container/build_tools/build_image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/set_env.sh
-
-$(aws ecr get-login --no-include-email --region us-west-2  --registry-ids 763104351884)
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} $DIR/..
