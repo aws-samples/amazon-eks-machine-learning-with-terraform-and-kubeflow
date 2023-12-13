a95edbce47c51a82bb28e6f70e2995a214c249fb
Ajay Vohra <ajayvohr@amazon.com>
2023-12-13 13:07:28 -0500

    update kubectl install

--- eks-cluster/install-kubectl-linux.sh
+++ eks-cluster/install-kubectl-linux.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.7/2020-07-08/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
 
 sudo mv ./kubectl /usr/local/bin/
 
-kubectl version --short --client
+kubectl version
 
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.7/2020-07-08/bin/linux/amd64/aws-iam-authenticator
+curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.11/aws-iam-authenticator_0.6.11_linux_amd64
 
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/
