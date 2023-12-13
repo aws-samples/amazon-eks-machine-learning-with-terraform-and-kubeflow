92b8207e22a0e51ec514dbd94c4835a5289d2189
Ajay Vohra <ajayvohr@amazon.com>
2023-12-13 14:57:17 -0500

    Fix #51

--- eks-cluster/terraform/aws-eks-cluster/aws-eks-cluster.tf
+++ eks-cluster/terraform/aws-eks-cluster/aws-eks-cluster.tf
@@ -107,12 +107,6 @@ provider "aws" {
   profile                 = var.profile
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 provider "kubectl" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
@@ -129,9 +123,28 @@ provider "kubectl" {
   }
 }
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.id]
+      command     = "aws"
+    }
+  }
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.id]
+    command     = "aws"
+  }
+}
+
 
 data "aws_caller_identity" "current" {}
 
