8ca12fa77b4b76463b6f43640680be4626951cca
Ajay Vohra <ajayvohr@amazon.com>
2025-08-13 12:54:59 -0400

    Fix #109

--- eks-cluster/terraform/aws-eks-cluster-and-nodegroup/versions.tf
+++ eks-cluster/terraform/aws-eks-cluster-and-nodegroup/versions.tf
@@ -11,5 +11,10 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
+    }
   }
 }
