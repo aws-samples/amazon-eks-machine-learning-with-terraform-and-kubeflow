7bc0e765910ce3397548c1f68fc2e6c61219cfda
Ajay Vohra <ajayvohr@amazon.com>
2025-12-26 17:33:14 -0500

    update triton inference server: neuron sdk 2.27.0

--- containers/tritonserver-neuronx-vllm/build_tools/set_env.sh
+++ containers/tritonserver-neuronx-vllm/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=tritonserver-neuronx-vllm
-export IMAGE_TAG=2.49.0-2.26.1-0.9.0.1
+export IMAGE_TAG=25.05-2.27.0-0.11.0
--- containers/tritonserver-neuronx/build_tools/set_env.sh
+++ containers/tritonserver-neuronx/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=tritonserver-neuronx
-export IMAGE_TAG=2.49-2.26.1
+export IMAGE_TAG=25.05-2.27.0
