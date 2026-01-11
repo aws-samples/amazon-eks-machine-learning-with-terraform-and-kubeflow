eeed1fa11036621f955ddab46dddb974459e8e52
ratnopamc <ratnopam@users.noreply.github.com>
2026-01-11 12:53:08 -0800

    Added kubectl autocomplete

--- eks-cluster/utils/install-kubectl-linux.sh
+++ eks-cluster/utils/install-kubectl-linux.sh
@@ -13,3 +13,13 @@ curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authen
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/
 aws-iam-authenticator help
+
+# Install bash-completion if not already installed
+sudo apt-get install -y bash-completion
+
+# Enable kubectl bash autocomplete
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+
+echo "kubectl autocomplete installed. Run 'source ~/.bashrc' or start a new shell to activate."
