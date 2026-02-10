3d0d07c6fbb789d65e38b1a51d4fdd0d8556678b
Ajay Vohra <ajayvohr@amazon.com>
2026-02-10 12:25:20 -0500

    update neuron sdk 2.27.1

--- containers/ray-pytorch-neuronx-vllm/build_tools/set_env.sh
+++ containers/ray-pytorch-neuronx-vllm/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=ray-neuronx-vllm
-export IMAGE_TAG=ray2.52.1-py312-2.27.0-0.11.0
+export IMAGE_TAG=ray2.52.1-py312-2.27.1-0.13.0
--- containers/ray-pytorch-neuronx/build_tools/set_env.sh
+++ containers/ray-pytorch-neuronx/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=ray-neuronx
-export IMAGE_TAG=ray2.52.1-py312-2.27.0
+export IMAGE_TAG=ray2.52.1-py312-2.27.1
