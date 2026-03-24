cbc5513c0fa2bc04c49ac7fa73b2915910df04a0
Ajay Vohra <ajayvohr@amazon.com>
2026-03-24 08:27:59 -0400

    update script

--- charts/machine-learning/training/pytorchjob-distributed/scripts/accelerate/text/cpt_accelerate.py
+++ charts/machine-learning/training/pytorchjob-distributed/scripts/accelerate/text/cpt_accelerate.py
@@ -78,6 +78,7 @@ class CPTConfig:
     # Other
     seed: int = 42
     num_workers: int = 4
+    gradient_checkpointing: bool = True
     
     @classmethod
     def from_args(cls, args: argparse.Namespace) -> 'CPTConfig':
@@ -237,8 +238,8 @@ def train(config: CPTConfig):
         seed=config.seed,
         dataloader_drop_last=False,
         ddp_find_unused_parameters=False,
-        gradient_checkpointing=True,
-        gradient_checkpointing_kwargs={"use_reentrant": False},
+        gradient_checkpointing=config.gradient_checkpointing,
+        gradient_checkpointing_kwargs={"use_reentrant": False} if config.gradient_checkpointing else None,
     )
     
     # Data collator
